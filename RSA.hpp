3fb6f31786f066e3d54dd739733c696003ae284d
Loge <85043963+xLoge@users.noreply.github.com>
2023-02-17 18:53:45 +0100

    Update RSA.hpp

--- RSA.hpp
+++ RSA.hpp
@@ -9,7 +9,6 @@
 #include <bitset>
 #include <random>
 #include <vector>
-#include <execution>
 #include <future>
 
 typedef boost::multiprecision::cpp_int number_t;
@@ -22,9 +21,10 @@ constexpr T pow(T base, T exp)
 {
     T res = 1;
     while (exp > 0) {
-        if (exp & 1)
+        if (exp & 1) {
             res = (res * base);
-        exp = exp >> 1;
+        }
+        exp >>= 1;
         base = (base * base);
     }
     return res;
@@ -36,9 +36,10 @@ constexpr T pow(T base, T exp, const T& mod)
     T res = 1;
     base = base % mod;
     while (exp > 0) {
-        if (exp & 1)
+        if (exp & 1) {
             res = (res * base) % mod;
-        exp = exp >> 1;
+        }
+        exp >>= 1;
         base = (base * base) % mod;
     }
     return res;
@@ -52,8 +53,9 @@ constexpr T pow(T base, T exp, const T& mod)
 template<class T>
 constexpr std::ostream& operator<<(std::ostream& out, const std::vector<T>& vec)
 {
-    for (size_t i = 0; i < vec.size() - 1; ++i)
+    for (size_t i = 0; i < vec.size() - 1; ++i) {
         out << vec[i] << ", ";
+    }
     return out << vec[vec.size() - 1];
 }
 
@@ -63,15 +65,18 @@ namespace RSA
 {
     class RSA
     {
-    private:
         constexpr static inline uint32_t DEFAULT_BITS = 4096;
         constexpr static inline uint32_t DEFAULT_BLOCK = 256;
         constexpr static inline uint32_t DEFAULT_STRENGHT = 25;
-
+    private:
+        bool bGood = false;
         uint32_t block = DEFAULT_BLOCK;
         uint32_t bits = DEFAULT_BITS;
-        bool bGood = false;
-        number_t p, q, n, phi, d, e;
+        uint32_t e = 65537;
+        number_t p, q;
+        number_t n;
+        number_t phi;
+        number_t d;
 
     public:
         std::array<number_t, 2> public_key;
@@ -88,33 +93,28 @@ namespace RSA
             }
         }
 
-        constexpr inline bool good() const noexcept
-        {
-            return bGood;
-        }
-
         constexpr operator bool() const noexcept
         {
             return bGood;
         }
 
-        constexpr inline uint32_t blocksize() const noexcept
+        constexpr uint32_t blocksize() const noexcept
         {
             return block;
         }
 
-        constexpr inline uint32_t keysize() const noexcept
+        constexpr uint32_t keysize() const noexcept
         {
             return bits;
         }
 
-        inline void set_and_init(const uint32_t key_bits = DEFAULT_BITS, const uint32_t block_size = DEFAULT_BLOCK, const uint32_t strenght = DEFAULT_STRENGHT)
+        void set_and_gen(const uint32_t key_bits = DEFAULT_BITS, const uint32_t block_size = DEFAULT_BLOCK, const uint32_t strenght = DEFAULT_STRENGHT)
         {
             set(key_bits, block_size);
-            init(strenght);
+            gen(strenght);
         }
 
-        inline void set(const uint32_t key_bits = DEFAULT_BITS, const uint32_t block_size = DEFAULT_BLOCK)
+        void set(const uint32_t key_bits = DEFAULT_BITS, const uint32_t block_size = DEFAULT_BLOCK)
         {
             if (!block_size || !key_bits || key_bits < block_size * 8 || key_bits % 8 != 0) {
                 throw std::runtime_error("");
@@ -124,21 +124,31 @@ namespace RSA
             }
         }
 
-        void init(const uint32_t strenght = DEFAULT_STRENGHT) noexcept
+        void gen(const uint32_t strenght = DEFAULT_STRENGHT) noexcept
         {
             static std::random_device rd;
             static std::mt19937_64 gen(rd());
             std::uniform_int_distribution<> dist(0, bits / 8);
+
+            bGood = false;
+            uint32_t e = 65537;
+            p = 0;
+            q = 0;
+            n = 0;
+            phi = 0;
+            d = 0;
 
             do
             {
                 uint32_t rand1 = dist(gen), rand2 = dist(gen);
 
                 while (rand1 % 8 != 0 || rand2 % 8 != 0) {
-                    if (rand1 % 8 != 0)
+                    if (rand1 % 8 != 0) {
                         ++rand1;
-                    if (rand2 % 8 != 0)
+                    }
+                    if (rand2 % 8 != 0) {
                         ++rand2;
+                    }
                 }
 
                 // generate p and q
@@ -155,16 +165,12 @@ namespace RSA
                 phi = (p - 1) * (q - 1);
 
                 // calculate e
-                if (gcd(65537, phi) != 1) {
-                    size_t i = 2;
-                    while (gcd(i, phi) != 1)
-                    {
+                if (gcd(e, phi) != 1) {
+                    uint32_t i = 2;
+                    while (gcd(i, phi) != 1) {
                         ++i;
                     }
                     e = i;
-                }
-                else {
-                    e = 65537;
                 }
 
                 //calculate d
@@ -177,31 +183,28 @@ namespace RSA
             bGood = true;
         }
 
-        std::vector<number_t> encrypt(const std::string str, const std::array<number_t, 2>& public_key)
+        std::vector<number_t> encrypt(const std::string_view& str, const std::array<number_t, 2>& public_key)
         {
-            const auto power = [](const number_t& num, const number_t& e, const number_t& n)
-            {
-                return pow(num, e, n);
+            const auto _pow = [public_key](const number_t& num) {
+                return pow(num, public_key[0], public_key[1]);
             };
 
             if (str.empty()) {
-                return std::vector<number_t>();
+                return std::vector<number_t>{ };
             }
 
             std::vector<number_t> result = create_block_vector(str, block);
-            std::vector< std::future<number_t> > threads;
+            std::vector<std::future<number_t>> threads;
             threads.reserve(result.size());
 
-            for (const auto& num : result)
-            {
-                threads.emplace_back(std::async(std::launch::async, power, num, public_key[0], public_key[1]));
+            for (const number_t& num : result) {
+                threads.push_back(std::async(_pow, num));
             }
 
             result.clear();
 
-            for (std::future<number_t>& thread : threads)
-            {
-                result.emplace_back(thread.get());
+            for (std::future<number_t>& thread : threads) {
+                result.push_back(thread.get());
             }
 
             return result;
@@ -209,77 +212,77 @@ namespace RSA
 
         std::string decrypt(const std::vector<number_t>& vec)
         {
-            const auto dec = [](const number_t& num, const number_t& d, const number_t& n, const uint32_t& blocksize)
+            auto _decrypt = [&](const number_t& num, const uint32_t blocksize) -> std::string
             {
-                const auto to_char = [](long long bits)
+                auto bits_to_char = [](long long _bits) -> char
                 {
-                    std::string bit = std::to_string(bits);
+                    std::string bits = std::to_string(_bits);
 
-                    for (auto& i : bit)
-                    {
-                        if (i == '1')
-                            i = '0';
-                        else
-                            i = '1';
+                    for (auto& bit : bits) {
+                        if (bit == '1') {
+                            bit = '0';
+                        }
+                        else {
+                            bit = '1';
+                        }
                     }
 
-                    bits = atoll(bit.c_str());
+                    _bits = std::atoll(bits.c_str());
 
-                    char dec = 0;
-                    int i = 0;
-                    int rem = 0;
+                    char result = 0;
+                    uint32_t i = 0, rem = 0;
 
-                    while (bits != 0)
-                    {
-                        rem = bits % 10;
-                        bits /= 10;
-                        dec += rem * pow(2, i);
+                    while (_bits != 0) {
+                        rem = _bits % 10;
+                        _bits /= 10;
+                        result += static_cast<char>(rem * pow(static_cast<uint32_t>(2), i));
                         ++i;
                     }
 
-                    return dec;
+                    return result;
                 };
 
-                std::string block(pow(num, d, n).str()), result;
+                std::string block = pow(num, d, n).str();
+                std::string result;
+                result.reserve(blocksize);
 
                 std::string_view view = block;
 
-                for (uint32_t i = 0; i < blocksize; ++i)
-                {
-                    const long long num = std::atoll(block.substr(0, 8).data());
+                for (uint32_t i = 0; i != blocksize; ++i) {
+                    const long long num = std::atoll(block.substr(0, 8).c_str());
                     block = view.substr(8);
                     view = block;
 
-                    if (num == 11111111)
+                    if (num == 11111111) {
                         continue;
+                    }
 
-                    result.push_back(to_char(num));
+                    result.push_back(bits_to_char(num));
                 }
 
                 return result;
             };
 
-            const size_t    blocks = vec.size();
-            const uint32_t  blocksize = block / 8;
+            const size_t blocks = vec.size();
+            const uint32_t blocksize = block / 8;
 
-            std::vector<std::future<std::string>> threads; threads.reserve(blocks);
+            std::vector<std::future<std::string>> threads;
+            threads.reserve(blocks);
 
-            for (const number_t& i : vec)
-            {
-                threads.emplace_back(std::async(dec, i, d, n, blocksize));
+            for (const number_t& i : vec) {
+                threads.push_back(std::async(_decrypt, i, blocksize));
             }
 
             std::string decrypted;
 
-            for (std::future<std::string>& thread : threads)
-            {
+            for (std::future<std::string>& thread : threads) {
                 decrypted.append(thread.get());
             }
 
             return decrypted;
         }
 
-   // private:
+    private:
 
         static std::vector<number_t> create_block_vector(const std::string_view& str, const uint32_t block)
         {
@@ -287,29 +290,25 @@ namespace RSA
 
             std::string msg;
 
-            for (const char& i : str)
-            {
-                const std::bitset<8> bits(i);
-                msg += bits.to_string();
+            for (const char& ch : str) {
+                const std::bitset<8> bits(ch);
+                msg.append(bits.to_string());
             }
-            for (auto& i : msg)
-            {
-                if (i == '0')
-                {
+            
+            for (auto& i : msg) {
+                if (i == '0') {
                     i = '1';
                 }
-                else
-                {
+                else {
                     i = '2';
                 }
             }
 
             size_t msg_size = msg.size() / 8;
 
-            while (msg_size % blocksize != 0)
-            {
+            while (msg_size % blocksize != 0) {
                 ++msg_size;
-                msg += "11111111";
+                msg.append("11111111", 8);
             }
 
             const size_t blocks = msg_size / blocksize;
@@ -318,11 +317,10 @@ namespace RSA
             std::vector<number_t> results;
             results.reserve(blocks);
 
-            std::string_view view(msg);
+            std::string_view view = msg;
 
-            for (size_t i = 0; i < blocks; ++i)
-            {
-                results.emplace_back(number_t(view.substr(0, get_size)));
+            for (size_t i = 0; i != blocks; ++i) {
+                results.emplace_back(view.substr(0, get_size));
                 msg = view.substr(get_size);
             }
 
@@ -332,7 +330,8 @@ namespace RSA
         static number_t egcd(const number_t& a, const number_t& b, number_t* x, number_t* y)
         {
             if (a == 0) {
-                *x = 0, * y = 1;
+                *x = 0;
+                *y = 1;
                 return b;
             }
 
